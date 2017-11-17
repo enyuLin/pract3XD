9e746b81955debc59ca2101811fd8824349653da
Dani <danidiezsanchez@gmail.com>
2017-11-17 13:11:10 +0100

    dani
    test

--- src/Types/Message.java
+++ src/Types/Message.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Message implements Serializable{
 
 	/**
-	 * 
+	 * message
 	 */
 	private static final long serialVersionUID = 1L;
 	private  boolean isRead;
